8503815f3349aed5e68a5b36b7237fc2b2bd2eea
jooyeon <jooyeon0907@naver.com>
2024-12-28 16:08:33 +0900

    redis 사용을 위한 bean 설정
    part09. 실전 배당금 프로젝트
    04. 서비스 구현

--- src/main/java/com/dayone/Application.java
+++ src/main/java/com/dayone/Application.java
@@ -2,10 +2,12 @@ package com.dayone;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling // spring boot 에서 스케쥴러를 사용하기 위해 추가
+@EnableCaching
 public class Application {
 
     public static void main(String[] args) {
