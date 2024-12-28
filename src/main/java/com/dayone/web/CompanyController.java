da646d433522a886a2eceba60b60d09f18b529b8
jooyeon <jooyeon0907@naver.com>
2024-12-28 23:09:23 +0900

    회사 삭제 기능 구현 -> 회사 정보, 배당금 정보, 자동 완성, 캐시 삭제
    part09. 실전 배당금 프로젝트
    05. 회원 관리

--- src/main/java/com/dayone/web/CompanyController.java
+++ src/main/java/com/dayone/web/CompanyController.java
@@ -2,8 +2,10 @@ package com.dayone.web;
 
 import com.dayone.entity.CompanyEntity;
 import com.dayone.model.Company;
+import com.dayone.model.constants.CacheKey;
 import com.dayone.service.CompanyService;
 import lombok.AllArgsConstructor;
+import org.springframework.cache.CacheManager;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
@@ -11,13 +13,14 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.util.ObjectUtils;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @RestController
 @RequestMapping("/company")
 @AllArgsConstructor
 public class CompanyController {
 	private final CompanyService companyService;
+
+	private final CacheManager redisCacheManager;
 
 	@GetMapping("/autocomplete")
 	public ResponseEntity<?> autocomplete(@RequestParam String keyword) {
@@ -50,9 +53,16 @@ public class CompanyController {
 		return ResponseEntity.ok(company);
 	}
 
-	@DeleteMapping
-	public ResponseEntity<?> deleteCompany(@RequestParam String ticker) {
-		return null;
+	@DeleteMapping("/{ticker}")
+	@PreAuthorize("hasRole('WRITE')")
+	public ResponseEntity<?> deleteCompany(@PathVariable String ticker) {
+		String companyName = this.companyService.deleteCompany(ticker);
+		this.clearFinanceCache(companyName);
+		return ResponseEntity.ok(companyName);
+	}
+
+	public void clearFinanceCache(String companyName) {
+		this.redisCacheManager.getCache(CacheKey.KEY_FINANCE).evict(companyName);
 	}
 
 }
