5a89a77b03dd7a6287cf0bb69a5b1ec13a67d7d8
jooyeon <jooyeon0907@naver.com>
2024-12-28 23:36:32 +0900

    로그 남기기
    part09. 실전 배당금 프로젝트
    06. 완성도 높이기

--- src/main/java/com/dayone/security/JwtAuthenticationFilter.java
+++ src/main/java/com/dayone/security/JwtAuthenticationFilter.java
@@ -31,11 +31,12 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
 	@Override
 	protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
 		String token = this.resolveTokenFromRequest(request);
-		System.out.println("token : " +token);
 
 		if (StringUtils.hasText(token) && this.tokenProvider.validateToken(token)) {
 			Authentication auth = this.tokenProvider.getAuthentication(token);
 			SecurityContextHolder.getContext().setAuthentication(auth);
+
+			log.info(String.format("[%s] -> %s", this.tokenProvider.getUsername(token), request.getRequestURI()));
 		}
 
 		filterChain.doFilter(request, response);
